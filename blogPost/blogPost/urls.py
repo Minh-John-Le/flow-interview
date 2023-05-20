e129c815e0279e3d16e45483f862729c49d02fe8
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-05-19 19:55:31 -0700

    complete display data

--- blogPost/blogPost/urls.py
+++ blogPost/blogPost/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from blogPostAPI import views as blogPostAPIViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blogPostAPI.urls')),
+    path('blogpostlist', blogPostAPIViews.blogPostList),
+    path('blogpostlist/<int:id>', blogPostAPIViews.blogPostDetail)
 ]
--- blogPost/blogPostAPI/views.py
+++ blogPost/blogPostAPI/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 
 # Create your views here.
 
-
+#======================= REST API for BLOG POST ===================================
 class BlogPostAPI(APIView):
     def get(self, request):
         items = BlogPost.objects.select_related('author').all()
@@ -23,3 +23,20 @@ class BlogPostAPI(APIView):
         serialized_item.save()
         return Response(serialized_item.data, status=status.HTTP_200_OK)
     
+
+#======================= BLOG POST VIEWS FUNCTION ===================================
+
+def blogPostList(request):
+
+    items = BlogPost.objects.select_related('author').all()
+
+    serialized_item = BlogPostSerializer(items, many = True)
+    
+    return render(request, "blogPostAPI/blog_post_list.html", {"data": serialized_item.data})
+
+
+def blogPostDetail(request, id):
+    items = BlogPost.objects.select_related('author').get(pk=id)
+    serialized_item = BlogPostSerializer(items)
+    return render(request, "blogPostAPI/blog_post_detail.html", {"item": serialized_item.data})
+ 
