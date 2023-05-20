d067ed3034eb360ebe70326019223c45836e3fe8
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-05-19 18:17:05 -0700

    create serialize class for REST

---
+++ blogPost/blogPostAPI/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from decimal import Decimal
+from django.contrib.auth.models import User
+from.models import BlogPost
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ['id', 'username']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer(read_only = True)
+    user_id = serializers.IntegerField(write_only = True)
+
+    class Meta:
+        model = BlogPost
+        fields = ['id', 'title', 'user', 'create_at','user_id']
